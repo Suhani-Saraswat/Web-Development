d2f802bd1bc1ec465e7ac058c7073f1915f412d5
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-06 21:05:00 +0530

    practice qs2

---
+++ DAY-11/practiceqs2.js
@@ -0,0 +1,13 @@
+// Create a traffic light system that shows what do to based on color.
+
+let color="yellow";
+
+if(color === "red"){
+    console.log("Stop! Light color is Red.");
+}else if(color === "yellow"){
+    console.log("Slow Down! Light color is Yellow.");
+}else if(color === "green"){
+    console.log("Go! Light color is Green.");
+}else {
+    console.log("Traffic light is broken!");
+}
