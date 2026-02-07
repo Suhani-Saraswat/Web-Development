d5eb3cc0b83c785aef4d37a36213ea283cee220d
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-07 19:43:59 +0530

    Practice Qs

---
+++ DAY-12/practiceqs6.js
@@ -0,0 +1,18 @@
+/* For the given string:
+    let msg = "help!";
+    Trim it & convert it to uppercase. */
+
+let msg = "help!";
+console.log(msg.trim().toUpperCase());
+
+/* For the String->let name = "ApnaCollege", predict the output for following:
+    name.slice(4,9)
+    name.indexOf("na")
+    name.replace("Apna","Our")
+*/
+
+let name = "ApnaCollege";
+
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna","Our"));
