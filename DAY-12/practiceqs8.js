65c67d2436579d516c2ab7162d4ca689852d4d0f
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-07 19:44:33 +0530

    Practice Qs

---
+++ DAY-12/practiceqs8.js
@@ -0,0 +1,9 @@
+/* Create a nested array to show the following tic-tac-toe game state.
+  X '' 0
+  '' X ''
+  0 '' X
+*/
+
+let game = [['X',null,'0'],[null,'X',null],['0',null,'X']];
+
+console.log(game);
