5fce11c08c7f8eccf4219badb2fcb805fcc9f900
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-08 20:53:26 +0530

    Even & Odd number

---
+++ DAY-13/Even&OddNo.js
@@ -0,0 +1,21 @@
+// Print Odd No from 1 to 20 in forward as well as backward direction
+
+console.log("Forward Direction:");
+for(let i=1; i<=20; i=i+2){
+    console.log(i);
+}
+console.log("Backward Direction:");
+for(let i=19; i>=1; i=i-2){
+    console.log(i);
+}
+
+// Print Even No from 1 to 20 in forward as well as backward direction
+
+console.log("Forward Direction:");
+for(let i=2; i<=20; i=i+2){
+    console.log(i);
+}
+console.log("Backward Direction:");
+for(let i=20; i>=1; i=i-2){
+    console.log(i);
+}
