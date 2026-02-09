af30b1abdbec9497103b64d5397f01ce1ed2f6bc
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-09 20:24:17 +0530

    Post

---
+++ DAY-14/post.js
@@ -0,0 +1,14 @@
+/* Create an object literal for the properties of thread/twitter post which includes-
+~username
+~content
+~likes
+~reposts
+~tags */
+
+const post = {
+    username: "@suhanisaraswat",
+    content: "This is my #firstpost",
+    likes: 550,
+    reposts: 15,
+    tags: ["@apnacollege","@sigma"]
+};
