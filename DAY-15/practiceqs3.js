cf897ab5b2bb207f80e1ff5c37b8a7e36fd4883d
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-10 21:55:49 +0530

    Average

---
+++ DAY-15/practiceqs3.js
@@ -0,0 +1,8 @@
+// Create a function that gives us the average of 3 numbers.
+
+function calcAvg(a, b, c) {
+    let avg = (a + b + c) / 3;
+    console.log(`Average of three numbers is ${avg}`);
+}
+
+calcAvg(90, 80, 100);
