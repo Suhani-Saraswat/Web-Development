0471143b1a1a5bb2757e7d5410f045a316c4ff6f
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-10 21:55:01 +0530

    Poem

---
+++ DAY-15/practiceqs1.js
@@ -0,0 +1,22 @@
+// Create a function that prints a poem.
+
+function printPoem() {
+    console.log("I am a human,");
+    console.log("I'm tired from cities,");
+    console.log("I am going to nature,");
+    console.log("to find peace.");
+    console.log("Long and Shady trees,");
+    console.log("and that cool Breeze,");
+    console.log("made me relax and calm,");
+    console.log("and found tree of palm.");
+    console.log("The plain ground,");
+    console.log("with flora and fauna,");
+    console.log("and silence all over the area,");
+    console.log("made me feel peace.");
+    console.log("The clouds were as light as air,");
+    console.log("And blew with the wind,");
+    console.log("And the sun set,");
+    console.log("Was the day's end");
+};
+
+printPoem();
