620c70a267193c821216da78829e23f7981828ba
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-14 23:30:41 +0530

    Form CSS

---
+++ DAY-19/form.js
@@ -0,0 +1,14 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.dir(form);
+
+    let user = this.elements[0]; //document.querySelector("#user");
+    let pass = this.elements[1]; //document.querySelector("#pass");
+
+    console.log(user.value);
+    console.log(pass.value);
+
+    alert(`Hi ${user.value}, your password is set to ${pass.value}`);
+});
