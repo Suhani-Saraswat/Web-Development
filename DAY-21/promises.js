c675928ec97376ab0077c2c021685a1dc5eb8a6c
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-16 21:06:58 +0530

    Promises JS

--- DAY-21/promises.js
+++ DAY-21/promises.js
@@ -33,3 +33,16 @@ saveToDb("Apna College",
         console.log("failure : weak connection. data not saved! ");
     }
 ); */
+
+// Using Promises function ->
+
+function saveToDb(data) {
+    return new Promise((resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+        if(internetSpeed > 4) {
+            resolve("success : data was saved");
+        }else {
+            reject("failure : weak connection");
+        }
+    });
+}
