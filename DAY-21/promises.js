f2263402fdc3727588df069882e5656d88adbe54
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-16 21:06:18 +0530

    Promises JS

---
+++ DAY-21/promises.js
@@ -0,0 +1,10 @@
+// Using Callback hell ->
+
+/* function saveToDb(data, success, failure) {
+    let internetSpeed = Math.floor(Math.random() * 10) +1;
+    if (internetSpeed > 4){
+        success();
+    } else {
+        failure();
+    }
+}
