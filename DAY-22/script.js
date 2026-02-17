f88f9b09f4f787a9c7462c92fa0ef20270c7d74a
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-17 21:54:49 +0530

    Script JS

---
+++ DAY-22/script.js
@@ -0,0 +1,6 @@
+let btnRef = document.querySelectorAll(".button-option");
+let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
+
