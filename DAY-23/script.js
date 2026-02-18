af2dab14858302a69369542c7f1abdcaa01eb110
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-18 19:49:51 +0530

    Script JS

--- DAY-23/script.js
+++ DAY-23/script.js
@@ -10,3 +10,15 @@ let erase = document.getElementById("erase");
 window.onload = () => {
   input.value = "";
 };
+
+//Access each class using forEach
+button_input.forEach((button_class) => {
+  button_class.addEventListener("click", () => {
+    if (equal_pressed == 1) {
+      input.value = "";
+      equal_pressed = 0;
+    }
+    //display value of each button
+    input.value += button_class.value;
+  });
+});
