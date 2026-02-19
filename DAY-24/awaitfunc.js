fdbc503e3818a132b948bad179a26f66c75e44f1
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-19 21:12:51 +0530

    Await Function JS

---
+++ DAY-24/awaitfunc.js
@@ -0,0 +1,9 @@
+function getNum() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let num = Math.floor(Math.random() * 10) + 1;
+            console.log(num);
+            resolve();
+        }, 1000);
+    });
+}
