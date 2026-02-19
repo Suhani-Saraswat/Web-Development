b185e2a1da168972707dc71862884b1416b75b04
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-19 21:11:24 +0530

    Async Function JS

--- DAY-24/asyncfunc.js
+++ DAY-24/asyncfunc.js
@@ -2,3 +2,12 @@ async function greet() {
     throw "404 page not found";
     return "hello!";
 }
+
+greet()
+  .then((result) => {
+    console.log("promise was resolved");
+    console.log("result was : ", result);
+  })
+  .catch((err) => {
+    console.log("promise was rejected with err : ",err);
+  });
