be1563dc0e0a94bb922dfac18ca08aaeee5c13c2
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-19 21:21:14 +0530

    First API Request With Async/Await Function JS

--- DAY-24/fetchwithasyncfunc.js
+++ DAY-24/fetchwithasyncfunc.js
@@ -5,3 +5,10 @@ async function getFacts() {
         let res1 = await fetch(url);
         let data1 = await res1.json();
         console.log("data1 - ", data1);
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log("data2 - ", data2);
+    }catch(e){
+        console.log("error - ", e);
+    }
+}
