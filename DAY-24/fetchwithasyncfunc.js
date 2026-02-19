c0bc3fe99606d23bbbd2f213539eeb6265316f19
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-19 21:20:57 +0530

    First API Request With Async/Await Function JS

--- DAY-24/fetchwithasyncfunc.js
+++ DAY-24/fetchwithasyncfunc.js
@@ -1,1 +1,7 @@
 let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+    try{
+        let res1 = await fetch(url);
+        let data1 = await res1.json();
+        console.log("data1 - ", data1);
