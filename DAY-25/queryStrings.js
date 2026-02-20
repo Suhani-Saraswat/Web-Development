f93939440ca27a72c800b6b7cdc99de06e554dbc
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-20 21:40:10 +0530

    Query Strings JS

--- DAY-25/queryStrings.js
+++ DAY-25/queryStrings.js
@@ -8,3 +8,15 @@ btn.addEventListener("click", async () => {
     let colArr = await getColleges(country);
     show(colArr);
 });
+
+function show(colArr) {
+    let list = document.querySelector("#list");
+    list.innerText = "";
+    for (col of colArr) {
+        console.log(col.name);
+
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
+}
