3aa0d871d52ab1aff35ecd1065d9b9b1e3c2f9a6
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-20 21:35:37 +0530

    Dog Picture API JS

--- DAY-25/dogPictAPI.js
+++ DAY-25/dogPictAPI.js
@@ -1,2 +1,8 @@
 let url = "https://dog.ceo/api/breeds/image/random";
 let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+    let link = await getImage();
+    let img = document.querySelector("#result");
+    img.setAttribute("src", link);
+});
