622db1c930ba8605b6a65e99c59d811c815860f5
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-20 21:33:16 +0530

    Axios JS

--- DAY-25/axios.js
+++ DAY-25/axios.js
@@ -9,3 +9,12 @@ btn.addEventListener("click", async () => {
 
 let url = "https://catfact.ninja/fact";
 
+async function getFacts() {
+    try {
+        let res = await axios.get(url);
+        return res.data.fact;
+    } catch (e) {
+        console.log("error - ", e);
+        return "No fact found";
+    }
+}
