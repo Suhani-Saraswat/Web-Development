76fbab737c73d698b4e07ad6adafcc245627836b
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-20 21:32:38 +0530

    Axios JS

--- DAY-25/axios.js
+++ DAY-25/axios.js
@@ -1,1 +1,8 @@
 let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+    let fact = await getFacts();
+    //console.log(fact);
+    let p = document.querySelector("#result");
+    p.innerText = fact;
+});
