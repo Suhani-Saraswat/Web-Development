0adeedb67e3522dc6fbf12f97ce11bc911ddce73
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-20 21:39:54 +0530

    Query Strings JS

--- DAY-25/queryStrings.js
+++ DAY-25/queryStrings.js
@@ -1,2 +1,10 @@
 let url = "http://universities.hipolabs.com/search?name=";
 let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+    let country = document.querySelector("input").value;
+    console.log(country);
+
+    let colArr = await getColleges(country);
+    show(colArr);
+});
